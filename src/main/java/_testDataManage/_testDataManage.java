9d2e19f6bded6d694fcc9cbd6a18fbf725624046
AswinBahulayan <39877799+AswinBahulayan@users.noreply.github.com>
2018-06-17 17:17:28 +0530

    New testdata excel added and function modified for code reusability

--- src/main/java/_testDataManage/_testDataManage.java
+++ src/main/java/_testDataManage/_testDataManage.java
@@ -10,19 +10,19 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class _testDataManage {
-	public static String getDataSheet(String sheetname, String ColumnName, String RowName, String FileName) {
+	public static String getDataSheetENV(String ColumnName, String RowName) {
 		Cell cell=null;
 		Row row=null;
 		int f=0,g=0,i=0,j=0;
 		String z=null;
 			try{
 				String filepath=System.getProperty("user.dir")+"\\TestData";
-				String filename=FileName;
+				String filename="Environment.xlsx";
 				File file=new File(filepath+"\\"+filename);
 				FileInputStream input=new FileInputStream(file);
 				//String fileExtensionName = filename.substring(filename.indexOf("."));
 				Workbook wb=new XSSFWorkbook(input);			
-				 Sheet sh=wb.getSheet(sheetname);
+				 Sheet sh=wb.getSheet("ENV");
 				int noofrows=sh.getLastRowNum();
 				System.out.println("number of rows: "+noofrows);
 				int lastcolumnno=sh.getRow(0).getLastCellNum();
@@ -46,11 +46,55 @@ public class _testDataManage {
 					}				
 				}
 				z=cell.getStringCellValue();
-				
+				wb.close();
 			}catch(Exception e)
 			{e.printStackTrace();}
 			return z;
 		}
+	
+	public static String getTestData(String ColumnName, String RowName) {
+		Cell cell=null;
+		Row row=null;
+		int f=0,g=0,i=0,j=0;
+		String z=null;
+			try{
+				String filepath=System.getProperty("user.dir")+"\\TestData";
+				String filename="FreeCRMDataSheet.xlsx";
+				File file=new File(filepath+"\\"+filename);
+				FileInputStream input=new FileInputStream(file);
+				//String fileExtensionName = filename.substring(filename.indexOf("."));
+				Workbook wb=new XSSFWorkbook(input);			
+				 Sheet sh=wb.getSheet("TestData");
+				int noofrows=sh.getLastRowNum();
+				System.out.println("number of rows: "+noofrows);
+				int lastcolumnno=sh.getRow(0).getLastCellNum();
+				for(i=0;i<=noofrows;i++)
+				{
+					for(j=0;j<=lastcolumnno-1;j++)
+					{
+						row=sh.getRow(i);
+						cell=row.getCell(j);
+						String d=cell.getStringCellValue();
+						if(d.equalsIgnoreCase(RowName))
+						{
+							f=cell.getRowIndex();
+						}
+						if(d.equals(ColumnName))
+						{
+							g=cell.getColumnIndex();						
+						}
+						row=sh.getRow(f);
+						cell=row.getCell(g);				
+					}				
+				}
+				z=cell.getStringCellValue();
+				wb.close();
+			}catch(Exception e)
+			{e.printStackTrace();}
+			return z;
+		}
+	
+	
 
 
 }
