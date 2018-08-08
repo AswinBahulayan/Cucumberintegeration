311a2621a8ae32018990c3163dbfd3e6ca9d6845
AswinBahulayan <aswinbahulayan@gmail.com>
2018-08-08 21:56:15 +0530

    File path changed

--- src/main/java/testRunner/TestRunnerLogin.java
+++ src/main/java/testRunner/TestRunnerLogin.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="C:\\Users\\ASWIN\\workspace\\CucumJvm\\src\\main\\java\\featureFile\\login.feature"
+		features=".//src//main//java//featureFile//login.feature"
 		,glue= {"stepDefinition"})
 
 public class TestRunnerLogin {
